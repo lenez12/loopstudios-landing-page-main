3abfae008520279b019ec87600252a079c448d11
jery lenas <jerylenas@gmail.com>
2022-09-28 14:49:24 +0700

    change resize observer method

--- js/main.js
+++ js/main.js
@@ -11,64 +11,64 @@ closeIcon.addEventListener("click", () => {
 
 var width = screen.width;
 
-var ro = new ResizeObserver((entries) => {
-	for (let entry of entries) {
-		const cr = entry.contentRect;
-		console.log("Element:", entry.target);
-		console.log(`Element size: ${cr.width}px x ${cr.height}px`);
-		console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
-	}
+// var ro = new ResizeObserver((entries) => {
+// 	for (let entry of entries) {
+// 		const cr = entry.contentRect;
+// 		console.log("Element:", entry.target);
+// 		console.log(`Element size: ${cr.width}px x ${cr.height}px`);
+// 		console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+// 	}
+// });
+
+let isMobile = width <= 767;
+let dir = isMobile ? "../images/mobile/" : "../images/desktop/";
+const Gallery = [
+	{
+		path: dir + "image-deep-earth.jpg",
+		caption: "Deep Earth",
+	},
+	{
+		path: dir + "image-night-arcade.jpg",
+		caption: "Night Arcade",
+	},
+	{
+		path: dir + "image-soccer-team.jpg",
+		caption: "Soccer Team VR",
+	},
+	{
+		path: dir + "image-grid.jpg",
+		caption: "The Grid",
+	},
+	{
+		path: dir + "image-from-above.jpg",
+		caption: "From Up Above VR",
+	},
+	{
+		path: dir + "image-pocket-borealis.jpg",
+		caption: "Pocket Borealis",
+	},
+	{
+		path: dir + "image-curiosity.jpg",
+		caption: "The curiosity",
+	},
+	{
+		path: dir + "image-fisheye.jpg",
+		caption: "Make it fisheye",
+	},
+];
+
+const gallery = document.getElementById("gallery");
+Gallery.forEach((item) => {
+	gallery.innerHTML += `
+ <figure id="gallery-figure">
+			<img src="${item.path}" alt="">
+			<figcaption>${item.caption}</figcaption>
+		</figure>
+`;
 });
 
-function onresize() {
-	var w = window.outerWidth;
-	let isMobile = w <= 767;
-	let dir = isMobile ? "../images/mobile/" : "../images/desktop/";
-	const Gallery = [
-		{
-			path: dir + "image-deep-earth.jpg",
-			caption: "Deep Earth",
-		},
-		{
-			path: dir + "image-night-arcade.jpg",
-			caption: "Night Arcade",
-		},
-		{
-			path: dir + "image-soccer-team.jpg",
-			caption: "Soccer Team VR",
-		},
-		{
-			path: dir + "image-grid.jpg",
-			caption: "The Grid",
-		},
-		{
-			path: dir + "image-from-above.jpg",
-			caption: "From Up Above VR",
-		},
-		{
-			path: dir + "image-pocket-borealis.jpg",
-			caption: "Pocket Borealis",
-		},
-		{
-			path: dir + "image-curiosity.jpg",
-			caption: "The curiosity",
-		},
-		{
-			path: dir + "image-fisheye.jpg",
-			caption: "Make it fisheye",
-		},
-	];
+// function onresize() {
+// 	var w = window.outerWidth;
+// }
 
-	const gallery = document.getElementById("gallery");
-
-	Gallery.forEach((item) => {
-		gallery.innerHTML += `
-     <figure id="gallery-figure">
-                <img src="${item.path}" alt="">
-                <figcaption>${item.caption}</figcaption>
-            </figure>
-    `;
-	});
-}
-
-onresize();
+// onresize();
